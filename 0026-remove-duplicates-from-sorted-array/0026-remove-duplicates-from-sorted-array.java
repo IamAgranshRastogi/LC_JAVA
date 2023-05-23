c73a0919ddf058ef03ae083872fc41a217279e23
AGRANSH RASTOGI <118201022+IamAgranshRastogi@users.noreply.github.com>
2023-05-23 20:56:06 +0530

    0026-remove-duplicates-from-sorted-array.java

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,12 +1,13 @@
 class Solution 
 {  public int removeDuplicates(int[] nums) 
-   { int i = 0;
-     for (int j = 1; j < nums.length; j++) 
-     { if (nums[i] != nums[j]) 
-       { i++;
-         nums[i] = nums[j];
-       }
-     }
-     return i + 1;
+   { Set<Integer> s=new TreeSet<Integer>();
+    for(int i=0;i<nums.length;i++)
+    { s.add(nums[i]);
+    }
+    int index=0;
+    for(int j:s)
+    { nums[index++]=j;
+    }
+    return index;
    }
 }
