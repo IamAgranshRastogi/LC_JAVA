bb9a618d8b0b9dadc71baca7e3fd571bd3f77f84
AGRANSH RASTOGI <118201022+IamAgranshRastogi@users.noreply.github.com>
2023-07-10 12:46:12 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (14.50%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,6 +1,7 @@
 class Solution 
 { public int findComplement(int num) 
   {  if(num==0) return 1;
+       // BitLength = MSB + 1
        int bitLen=(int)(Math.log(num)/Math.log(2)) + 1 ;
        int msk=(1<<(bitLen)) -1;
        return msk^num;
