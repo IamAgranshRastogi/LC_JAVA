8dc61777b185d3b8401ca6a2d3c25cde40379111
AGRANSH RASTOGI <118201022+IamAgranshRastogi@users.noreply.github.com>
2023-08-02 20:52:29 +0530

    Time: 2 ms (80.77%), Space: 55 MB (95.82%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -2,12 +2,10 @@ class Solution
 { public int getCommon(int[] nums1, int[] nums2) 
   { int i=0;
     int j=0;
-    int res=Integer.MAX_VALUE;
+   
     while(i<nums1.length && j<nums2.length)
     { if(nums1[i]==nums2[j])
-      { res=Math.min(res,nums1[i]);
-        i++;
-        j++;
+      { return nums1[i];
       }
       else if(nums1[i]<nums2[j])
       { i++;
@@ -17,6 +15,6 @@ class Solution
       { j++;
       }
     }   
-   return res==Integer.MAX_VALUE?-1:res;
+   return -1;
   }
 }
