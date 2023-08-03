89408de5fcb9ceb81fed035c53848baa1d7ad5fb
AGRANSH RASTOGI <118201022+IamAgranshRastogi@users.noreply.github.com>
2023-08-03 15:56:04 +0530

    brute force approach

--- 1089-duplicate-zeros/1089-duplicate-zeros2.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros2.java
@@ -1,5 +1,11 @@
-//Brute Force Approach 
-// TC: O(n^2)
+/* Brute Force Approach 
+     * (1) Traverse array till length-1.
+     * (2) If the element where you current at is zero then shift all the elements
+           including zero to one step ahead.
+     * (3) Don't forget increment the pointer by 1 else array will filled by 0s.
+     * Time Complexity = O(n^2) for traversing & shifting.
+     * Space Complexity = 0(1).
+*/
 class Solution 
 { public void duplicateZeros(int[] arr) 
   { int n=arr.length;
