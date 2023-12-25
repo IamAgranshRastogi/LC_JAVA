6960d255c5bc4dc60ad386a178c3e4f6991df58e
AGRANSH RASTOGI <118201022+IamAgranshRastogi@users.noreply.github.com>
2023-12-25 22:26:00 +0530

    Time: 1 ms (96.63%), Space: 44.4 MB (12.87%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,16 +1,14 @@
-//TC:O(n)
-//SC:O(n)
-
-//Two pointer Approach
-class Solution 
-{  public int removeDuplicates(int[] nums) 
-   { int i = 0;  // For Bringing all uniq ele to start
-     for (int j = 1; j < nums.length; j++)
-     { if (nums[i] != nums[j]) 
-       { i++;
-         nums[i] = nums[j];
-       }
-     }
-     return i + 1;
-   }
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++){
+            if (nums[i] != nums[j]){
+               i++;
+               nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
 }
+// TC:O(n)
+//SC:O(1)
