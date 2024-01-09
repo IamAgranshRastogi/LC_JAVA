c168dcd499746ecb76bca69d8489cab438ae8208
AGRANSH RASTOGI <118201022+IamAgranshRastogi@users.noreply.github.com>
2024-01-09 16:15:11 +0530

    Time: 1 ms (99.83%), Space: 51 MB (6.43%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,23 @@
+// Using Boyer-Moore Voting Approach
+// TC : O(n) 
+// SC : O(1)
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int cnt = 0;
+        int mjrcandidate = 0;
+        for(int i=0; i<n;i++) {
+          if(cnt==0) {
+             mjrcandidate = nums[i]; 
+          }
+          if(nums[i] ==mjrcandidate) {
+             cnt++;
+          }
+          else {
+             cnt--;
+          }
+        }
+        return mjrcandidate;
+    }
+}
