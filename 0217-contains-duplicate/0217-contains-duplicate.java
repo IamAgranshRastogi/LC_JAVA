0672849df1235e7488b113f4f1a3980818dd0763
AGRANSH RASTOGI <118201022+IamAgranshRastogi@users.noreply.github.com>
2024-01-31 20:04:00 +0530

    Time: 12 ms (53.51%), Space: 60.7 MB (16.09%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,3 +1,7 @@
+/*
+//                                   1.Sorting Approach
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
@@ -9,8 +13,25 @@ class Solution {
         return false;
     }
 }
+*/
 
-// Sorting Approach
 
-// Time Complexity: O(nlogn)
-// Space Complexity: O(1)
+//                                   2. HashMap Approach
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                return true;
+            }
+            map.put(nums[i],1);
+        }
+        return false;
+    }
+}
+
+
+
